314de911dbe49300195776566d2d2ffbc7dbadf5
Meghashyam Adoni <meghashyam@hoodoo.digital>
2020-05-06 12:21:12 -0700

    Format html written to file

--- utils.js
+++ utils.js
@@ -1,3 +1,8 @@
+const fs = require('fs')
+const fsPromises = fs.promises
+const cheerio = require('cheerio')
+const prettier = require('prettier')
+
 const getContentUrl = (path) => {
     return path + '.html?wcmmode=disabled'
 }
@@ -6,5 +11,26 @@ const getJsonUrl = (path) => {
     return path + '.infinity.json'
 }
 
+const writeToFile = async (path, data) => {
+    let fileHandle = null
+    try {
+        fileHandle = await fsPromises.open(path, 'w')
+        await fileHandle.writeFile(data)
+    } finally {
+        if (fileHandle) {
+            await fileHandle.close()
+        }
+    }
+}
+
+const tidy = (html) => {
+    const $ = cheerio.load(html, {
+        xmlMode: true
+    })
+    return prettier.format($.html(), { parser: 'html' })
+}
+
 exports.getContentUrl = getContentUrl
 exports.getJsonUrl = getJsonUrl
+exports.writeToFile = writeToFile
+exports.tidy = tidy
