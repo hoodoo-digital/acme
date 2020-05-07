9079abbfabe7d0e0d1ca0778816a3ee1e7f8bca2
Meghashyam Adoni <meghashyam@hoodoo.digital>
2020-05-06 17:51:39 -0700

    Add test resources

--- utils.test.js
+++ utils.test.js
@@ -1,15 +1,13 @@
+const fs = require('fs')
 const utils = require('./utils')
 
 test('returns formatted html', () => {
-    const actual = `
-    <div>
-<span>Some HTML here</span>
-  </div>
-    `
-    const expected = `<div>
-  <span>Some HTML here</span>
-</div>
-`
+    const options = {
+        encoding: 'utf-8',
+        flag: 'r'
+    }
+    const actual = fs.readFileSync('test/resources/unformatted.html', options)
+    const expected = fs.readFileSync('test/resources/formatted.html', options)
 
     expect(utils.tidy(actual)).toBe(expected)
 })
