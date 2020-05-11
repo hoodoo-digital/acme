be766551a5a0838357bb798a5cb8dee86259a61a
Meghashyam Adoni <meghashyam@hoodoo.digital>
2020-05-11 16:19:15 -0700

    Initial implementation of init command

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
 #!/usr/bin/env node
 
 const yargs = require('yargs')
+const prompts = require('prompts')
+const debug = require('debug')
+
 const client = require('./client')
 const { createStories, generatePreviewHeadHtml } = require('./create')
+const { writeToFile } = require('./utils')
 
 // eslint-disable-next-line no-unused-expressions
 yargs
+    .command('init', 'Generate the config file', async () => {
+        const log = debug('acme:init')
+        const questions = [
+            {
+                type: 'text',
+                name: 'username',
+                message: 'What is your AEM username?',
+                initial: 'admin'
+            },
+            {
+                type: 'text',
+                name: 'password',
+                message: 'What is your AEM password?',
+                initial: 'admin'
+            },
+            {
+                type: 'text',
+                name: 'baseURL',
+                message: 'What is the base URL of your AEM instance?',
+                initial: 'http://localhost:4502'
+            },
+            {
+                type: 'text',
+                name: 'storybookContentPath',
+                message:
+                    'What is the content path to the Storybook sample content?',
+                initial: '/content/<my-site>/storybook-sample-content'
+            },
+            {
+                type: 'text',
+                name: 'componentsContainer',
+                message:
+                    'What is the JCR path to the component variations on each page?',
+                initial: 'jcr:content/root/container'
+            },
+            {
+                type: 'text',
+                name: 'policyPath',
+                message: 'What is the path to the component policies?',
+                initial:
+                    '/conf/<my-site>/settings/wcm/policies/<my-site>/components'
+            }
+        ]
+        const response = await prompts(questions)
+        writeToFile('acme.settings.json', JSON.stringify(response, null, 4))
+        log('Generated "acme.settings.json"')
+    })
     .command(
         'pull',
         'Pull content from AEM',
