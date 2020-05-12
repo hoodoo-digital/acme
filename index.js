67377192299b854db7f002d901076d3fa68329aa
Meghashyam Adoni <meghashyam@hoodoo.digital>
2020-05-11 17:25:46 -0700

    Re-organize files

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const yargs = require('yargs')
 
 // eslint-disable-next-line no-unused-expressions
 yargs
-    .commandDir('commands')
+    .commandDir('src/commands')
     .usage('Usage: $0 <command> <options>')
     .demandCommand(1, 'You need at least one command before moving on').argv
