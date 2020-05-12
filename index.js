e7037b7a58f4ef15a990a8741fdbebef79cfff98
Meghashyam Adoni <meghashyam@hoodoo.digital>
2020-05-12 11:50:30 -0700

    Feature/add init command (#28)
    * Initial implementation of init command
    
    * Re-organized command modules
    
    * Re-organize files
    
    * Add sample settings file

--- index.js
+++ index.js
@@ -1,58 +1,9 @@
 #!/usr/bin/env node
 
 const yargs = require('yargs')
-const client = require('./client')
-const { createStories, generatePreviewHeadHtml } = require('./create')
 
 // eslint-disable-next-line no-unused-expressions
 yargs
-    .command(
-        'pull',
-        'Pull content from AEM',
-        (yargs) => {
-            return yargs
-                .option('destination', {
-                    alias: 'd',
-                    describe: 'Destination directory for AEM assets',
-                    type: 'string',
-                    default: 'aem-assets'
-                })
-                .config()
-                .demandOption('config')
-        },
-        (argv) => {
-            const username = argv.username || 'admin'
-            const password = argv.password || 'admin'
-            const credentials = username + ':' + password
-
-            client.init({
-                credentials: credentials,
-                baseURL: argv.baseURL,
-                storybookContentPath: argv.storybookContentPath,
-                componentsContainer: argv.componentsContainer,
-                assetsDir: argv.destination,
-                policyPath: argv.policyPath
-            })
-            client.getAllComponents()
-            client.getPolicies()
-            client.getResources()
-        }
-    )
-    .command(
-        'create',
-        'Create stories from AEM content',
-        {
-            source: {
-                alias: 's',
-                describe: 'Path to downloaded AEM assets',
-                type: 'string',
-                default: 'aem-assets'
-            }
-        },
-        (argv) => {
-            createStories(argv.source)
-            generatePreviewHeadHtml(argv.source)
-        }
-    )
+    .commandDir('src/commands')
     .usage('Usage: $0 <command> <options>')
     .demandCommand(1, 'You need at least one command before moving on').argv
---
+++ src/commands/pull.js
@@ -0,0 +1,34 @@
+const client = require('../client')
+
+module.exports = {
+    command: 'pull',
+    describe: 'Pull content from AEM',
+    builder: (yargs) => {
+        return yargs
+            .option('destination', {
+                alias: 'd',
+                describe: 'Destination directory for AEM assets',
+                type: 'string',
+                default: 'aem-assets'
+            })
+            .config()
+            .demandOption('config')
+    },
+    handler: (argv) => {
+        const username = argv.username || 'admin'
+        const password = argv.password || 'admin'
+        const credentials = username + ':' + password
+
+        client.init({
+            credentials: credentials,
+            baseURL: argv.baseURL,
+            storybookContentPath: argv.storybookContentPath,
+            componentsContainer: argv.componentsContainer,
+            assetsDir: argv.destination,
+            policyPath: argv.policyPath
+        })
+        client.getAllComponents()
+        client.getPolicies()
+        client.getResources()
+    }
+}
