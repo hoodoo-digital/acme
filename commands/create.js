3a009ec24fd22b83e2c35baf8ba012533a39d0b3
Meghashyam Adoni <meghashyam@hoodoo.digital>
2020-05-11 17:16:10 -0700

    Re-organized command modules

---
+++ commands/create.js
@@ -0,0 +1,18 @@
+const { createStories, generatePreviewHeadHtml } = require('./create')
+
+module.exports = {
+    command: 'create',
+    describe: 'Create stories from AEM content',
+    builder: {
+        source: {
+            alias: 's',
+            describe: 'Path to downloaded AEM assets',
+            type: 'string',
+            default: 'aem-assets'
+        }
+    },
+    handler: (argv) => {
+        createStories(argv.source)
+        generatePreviewHeadHtml(argv.source)
+    }
+}
