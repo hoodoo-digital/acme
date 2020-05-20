f8ea32d488266ce0b3061015e63a98991e0b2ba8
Meghashyam Adoni <meghashyam@hoodoo.digital>
2020-05-20 10:17:43 -0700

    Refactor and add error handling

--- src/commands/create.js
+++ src/commands/create.js
@@ -1,4 +1,12 @@
+const log = require('debug')('acme:create')
 const { createStories, generatePreviewJS } = require('create')
+
+const errorHandler = (err) => {
+    log(err.message)
+
+    // eslint-disable-next-line no-process-exit
+    process.exit(1)
+}
 module.exports = {
     command: 'create',
     describe: 'Create stories from AEM content',
@@ -10,8 +18,8 @@ module.exports = {
             default: 'aem-assets'
         }
     },
-    handler: (argv) => {
-        createStories(argv.source)
-        generatePreviewJS(argv.source)
+    handler: async (argv) => {
+        await createStories(argv.source).catch(errorHandler)
+        await generatePreviewJS(argv.source).catch(errorHandler)
     }
 }
