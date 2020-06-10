6863e0ba507dd0eff918d9018ed495c04cc4656e
Meghashyam Adoni <meghashyam@hoodoo.digital>
2020-06-10 11:14:37 -0700

    Add error handling and docs

--- src/acme-utils.js
+++ src/acme-utils.js
@@ -4,9 +4,18 @@ const acmeFetch = async (path) => {
     return response.text()
 }
 
+/**
+ * Retrieves the url for a given artboard
+ *
+ * @param {Array} data - Array of artboard objects
+ * @param {String} name - Name of artboard
+ * @returns {String} - Url of requested artboard; empty if artboard is not in the list or data is undefined/null
+ */
 const getArtboardUrl = (data, name) => {
-    const obj = data.find((item) => (item.name === name ? item.url : ''))
-    return obj && obj.url
+    if (!data) {
+        return ''
+    }
+    return data.find((item) => (item.name === name ? item.url : ''))
 }
 
 exports.acmeFetch = acmeFetch
