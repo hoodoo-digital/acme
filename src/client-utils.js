e9ce6625ee700a0c0264c4f3d6bff87b268e6e47
Meghashyam Adoni <meghashyam@hoodoo.digital>
2020-06-12 11:29:59 -0700

    Remove unwanted file

--- src/client-utils.js
+++
@@ -1,6 +0,0 @@
-const getArtboardUrl = (name) => {
-    const artboards = require(this.filePath)
-    return artboards.find((item) => (item.name === name ? item.url : ''))
-}
-
-exports.getArtboardUrl = getArtboardUrl
